67cd1d5d3604259d8438e7831865db27654a2353
Dan <quarkhopper@gmail.com>
2025-03-19 23:14:55 -0700

    initial commit

---
+++ tests/test_core.py
@@ -0,0 +1,10 @@
+import pytest
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from kitecore.core import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
